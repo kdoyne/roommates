b953914ad33bfb3a82ee576c4522247ff0a6900c
kdoyne <kdoyne930@gmail.com>
2014-04-15 11:37:25 -0400

    moving shopping items

--- app/controllers/sms_controller.rb
+++ app/controllers/sms_controller.rb
@@ -35,6 +35,7 @@ class SmsController < ApplicationController
           @bill.name = @item.item
           @bill.save 
           @item.destroy
+          reply(name.join(" "))
 
         else
           # searches database for events from that house that are on the same date
